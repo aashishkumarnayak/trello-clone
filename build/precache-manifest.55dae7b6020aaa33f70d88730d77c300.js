af42e9420352b17e9aaf1f83468d6c090074f06b
Aashish Kumar Nayak <kumaraashish57627@gmail.com>
2023-09-19 13:28:52 +0530

    ..

---
+++ build/precache-manifest.55dae7b6020aaa33f70d88730d77c300.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca10120285b52733c61f",
+    "url": "/static/css/main.de65e902.chunk.css"
+  },
+  {
+    "revision": "ca10120285b52733c61f",
+    "url": "/static/js/main.ca101202.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.42ac5946.js"
+  },
+  {
+    "revision": "66d44a74ea53507479aa",
+    "url": "/static/js/2.66d44a74.chunk.js"
+  },
+  {
+    "revision": "05803e46db862d8548536c4a79d3feff",
+    "url": "/index.html"
+  }
+];
